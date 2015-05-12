b1ad23114b9959e28612fbb46db77982e529d479
Jessica Engel <jessicaepdx@gmail.com>
2015-05-11 18:40:15 -0700

    go to individual project page with project link

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require('./lib/project')
 also_reload('lib/**/*.rb')
 
 get('/') do
-  @divisions = Division.all()
+  @all_divisions = Division.all()
   @all_employees = Employee.all()
   @all_projects = Project.all()
   erb(:index)
@@ -15,7 +15,7 @@ end
 
 post('/') do
   Division.create({:name => params.fetch("name")})
-  @divisions = Division.all()
+  @all_divisions = Division.all()
   @all_employees = Employee.all()
   @all_projects = Project.all()
   erb(:index)
@@ -36,7 +36,7 @@ end
 delete('/') do
   @division = Division.find(params.fetch("id").to_i)
   @division.delete
-  @divisions = Division.all
+  @all_divisions = Division.all
   @all_employees = Employee.all
   @all_projects = Project.all
   erb(:index)
@@ -53,10 +53,6 @@ get('/employee/:id') do
   erb(:employee)
 end
 
-post('/project/new') do
-  Project.create({:description => params.fetch("description")})
-  @divisions = Division.all()
-  @all_employees = Employee.all()
-  @all_projects = Project.all()
-  erb(:index)
+get('/project/:id') do
+  erb(:project)
 end
